33fa20d591fb42ecec6a49e6e963e9800ebd310f
MandavaMounika <147843734+MandavaMounika@users.noreply.github.com>
2026-01-12 22:36:19 +0530

    Delete src directory

--- src/api/auth.js
+++
@@ -1,9 +0,0 @@
-import api from "./axios";
-
-export const registerUser = (data) => {
-  return api.post("/auth/register", data);
-};
-
-export const loginUser = (data) => {
-  return api.post("/auth/login", data);
-};
--- src/pages/trainer/TrainerDashboard.jsx
+++
@@ -1,8 +0,0 @@
-export default function TrainerDashboard() {
-  return (
-    <div className="page">
-      <h1>Trainer Dashboard</h1>
-      <p>Manage trainees, publish blogs and track user progress.</p>
-    </div>
-  );
-}
